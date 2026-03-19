de6de5323af4ba1848e26376f4ce380e14d4471f
lorenzo <lpalaciosuttor@gmail.com>
2026-03-19 15:08:49 +0100

    inline image

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -4,5 +4,6 @@ import section from "./section";
 import theme from "./theme";
 import homepage from "./homepage";
 import pullQuote from "./pullQuote";
+import imageBlock from "./imageBlock";
 
-export const schemaTypes = [article, author, section, theme, homepage, pullQuote];
+export const schemaTypes = [article, author, section, theme, homepage, pullQuote, imageBlock];
