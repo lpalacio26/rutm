35694483a7fe6aae3d4238886a1e104246ec5878
lpalacio26 <lpalaciosuttor@gmail.com>
2026-04-01 19:43:49 +0200

    body thing

--- studio/schemaTypes/aboutPage.ts
+++ studio/schemaTypes/aboutPage.ts
@@ -19,10 +19,37 @@ export default defineType({
       initialValue: "Cambridge Advanced Learner's Dictionary & Thesaurus © Cambridge University Press",
     }),
     defineField({
-      name: "body",
-      title: "Body text",
-      type: "array",
-      of: [{ type: "block" }],
-    }),
+  name: "body",
+  title: "Body text",
+  type: "array",
+  of: [
+    {
+      type: "block",
+      marks: {
+        annotations: [
+          {
+            name: "link",
+            type: "object",
+            title: "Link",
+            fields: [{ name: "href", type: "url", title: "URL" }],
+          },
+          {
+            name: "footnote",
+            type: "object",
+            title: "Footnote",
+            fields: [
+              {
+                name: "note",
+                title: "Note",
+                type: "text",
+                rows: 3,
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+}),
   ],
 })
