f6be865834458b2fafd7a0d80236fdc3adb27be7
lpalacio26 <lpalaciosuttor@gmail.com>
2026-04-01 19:55:49 +0200

    change cdn value

--- astro/src/lib/sanity.ts
+++ astro/src/lib/sanity.ts
@@ -4,5 +4,5 @@ export const sanity = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
   apiVersion: "2025-01-01",
-  useCdn: true, // fast for published content
+  useCdn: false,
 });
