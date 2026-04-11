2452615e7bc05a3af22e02fd91660708d4770663
lpalacio26 <lpalaciosuttor@gmail.com>
2026-04-11 16:45:39 +0200

    redirection fix

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: 'https://www.rutmmag.com',
   output: "server",
   adapter: vercel(),
+  integrations: [sitemap()],
 });
